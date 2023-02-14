4366279b8cc8187f89c249a390a1c314f9338fa4
Shravanth Reddy Reddy <shrvnthrdy@gmail.com>
2023-02-13 21:49:25 -0500

    Modified contact screen and minor modifications to about, project screen

--- src/Screens/ContactScreen/ContactScreen.js
+++ src/Screens/ContactScreen/ContactScreen.js
@@ -1,10 +1,15 @@
 import { useNavigate } from 'react-router-dom';
+import './ContactScreen.css'
 
 function ContactScreen() {
     const navigate = useNavigate();
     function onClickHandle(id) {
         if (id === 1) {
             navigate("/")
+        } else if (id === 2) {
+            navigate("/projects")
+        } else if (id === 3) {
+            navigate("/about")
         }
     }
 
@@ -12,21 +17,40 @@ function ContactScreen() {
     <div className='about-main-container'>
         <nav className='about-nav-bar'>
             <div className='nav-bar-div'>
-                <span className='text-focus-in-about'>projects</span>
+                <span onClick={() => onClickHandle(2)} className='text-focus-in-about'>projects</span>
             </div>
             <div>
-                <span className='main-nav animate-about-nav'>contact</span>
+                <span className='main-nav contact-nav animate-about-nav'>Contact</span>
             </div>
             <div className='nav-bar-div'>
-                <span className='text-focus-in-about'>about</span>
+                <span onClick={() => onClickHandle(3)} className='text-focus-in-about'>about</span>
             </div>
         </nav>
-        <main className='about-secondary-container animate-in-up'>
+        <main className='about-secondary-container contact-screen-container animate-in-up'>
             <header className='header-about'>
                 <div>
                     <span onClick={() => onClickHandle(1)} className='programming-syntax hover-properties'>function about()<br/>&nbsp;&nbsp;&nbsp;return (</span>
                 </div>
             </header>
+            <div>
+                <div>
+                    <span className='animate-hello-contact'>Hello.</span>
+                </div>
+                <div>
+                    <p className='animate-p-contact'>
+                        Thanks for coming here. If you want to hire me for a job or interested in knowing more about me, you can contact me using the below ways.
+                        <br></br>
+                        <br></br>
+                        Email: <a href="mailto:shravanthreddy24@gmail.com">shravanthreddy24@gmail.com </a>
+                        <br></br>
+                        Social: <a href="https://www.linkedin.com/in/shravanth-reddy-reddy/">Linkedin </a>
+                        /
+                                <a href='https://github.com/ShravanthReddy'> Github </a>
+                        /
+                                <a href='https://twitter.com/shrvnth_rdy'> Twitter </a>
+                    </p>
+                </div>               
+            </div>
 
         </main>
     </div>
